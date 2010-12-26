f5720a66aca57f820da3283b669975bb0d806bf6
BlazejOwczarczyk <blazej.owczarczyk@gmail.com>
2010-12-27 00:59:16 +0100

    Module settings implementation

--- js/nicemessages.js
+++ js/nicemessages.js
@@ -5,15 +5,15 @@
  */
 Drupal.behaviors.nicemessages = {
   attach: function () {
+    jQuery.jGrowl.defaults.position = Drupal.settings.nicemessages.position;
     if (Drupal.settings.nicemessages.items) {
       for (i in Drupal.settings.nicemessages.items) {
         jQuery.jGrowl(Drupal.settings.nicemessages.items[i].content, {
-          life: 4000,
+          life: Drupal.settings.nicemessages.items[i].life,
+          glue: Drupal.settings.nicemessages.items[i].glue,
+          speed: Drupal.settings.nicemessages.items[i].speed,
           theme: Drupal.settings.nicemessages.items[i].type,
-          sticky: jQuery('a', Drupal.settings.nicemessages.items[i].content).size() || 
-            Drupal.settings.nicemessages.items[i].type == 'error' || 
-            Drupal.settings.nicemessages.items[i].type == 'warning' || 
-            jQuery(Drupal.settings.nicemessages.items[i].content).text().length > 100 ? true : false
+          sticky: Drupal.settings.nicemessages.items[i].life == 0
         });
       }
       delete Drupal.settings.nicemessages.items;
