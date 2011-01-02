e572dc150fd073cfc7a38d2c3fc4c99bcf84e7a0
Blazej <Blazej>
2011-01-02 03:53:19 +0100

    Fixed javascript error: Druapl.behaviors.nicemessages is undefined

--- js/nicemessages.js
+++ js/nicemessages.js
@@ -7,22 +7,24 @@
  */
 
 /**
- *  Displays messages with jGrowl.  
+ *  Displays messages with jGrowl.
  */
 Drupal.behaviors.nicemessages = {
   attach: function () {
-    jQuery.jGrowl.defaults.position = Drupal.settings.nicemessages.position;
-    if (Drupal.settings.nicemessages.items) {
-      for (i in Drupal.settings.nicemessages.items) {
-        jQuery.jGrowl(Drupal.settings.nicemessages.items[i].content, {
-          life: Drupal.settings.nicemessages.items[i].life,
-          glue: Drupal.settings.nicemessages.items[i].glue,
-          speed: Drupal.settings.nicemessages.items[i].speed,
-          theme: Drupal.settings.nicemessages.items[i].type,
-          sticky: Drupal.settings.nicemessages.items[i].life == 0
-        });
+    if (Drupal.settings.nicemessages) {
+      jQuery.jGrowl.defaults.position = Drupal.settings.nicemessages.position;
+      if (Drupal.settings.nicemessages.items) {
+        for (i in Drupal.settings.nicemessages.items) {
+          jQuery.jGrowl(Drupal.settings.nicemessages.items[i].content, {
+            life: Drupal.settings.nicemessages.items[i].life,
+            glue: Drupal.settings.nicemessages.items[i].glue,
+            speed: Drupal.settings.nicemessages.items[i].speed,
+            theme: Drupal.settings.nicemessages.items[i].type,
+            sticky: Drupal.settings.nicemessages.items[i].life == 0
+          });
+        }
+        delete Drupal.settings.nicemessages.items;
       }
-      delete Drupal.settings.nicemessages.items;
     }
   }
 }
